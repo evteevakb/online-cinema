3200964f3e09bcc695dfd7efaef2150481fd5207
denis <rden2000@gmail.com>
2025-04-21 16:55:30 +0300

    Formatting and fix schema

--- services/auth/src/db/postgre.py
+++ services/auth/src/db/postgre.py
@@ -11,12 +11,17 @@ from core.config import PostgreSettings
 Base = declarative_base()
 
 db_settings = PostgreSettings()
-dsn = (
+dsn_sync = (
+    f"postgresql+psycopg2://{db_settings.user}:{db_settings.password}"
+    + f"@{db_settings.host}:{db_settings.port}"
+    + f"/{db_settings.db}"
+)
+dsn_async = (
     f"postgresql+asyncpg://{db_settings.user}:{db_settings.password}"
     + f"@{db_settings.host}:{db_settings.port}"
     + f"/{db_settings.db}"
 )
-engine = create_async_engine(dsn, echo=True, future=True)
+engine = create_async_engine(dsn_async, echo=True, future=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
