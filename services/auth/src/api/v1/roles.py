85d6145b7c8379aa9318f38028665eb29be3a683
Ksenia Evteeva <evteeva.kb@gmail.com>
2025-04-24 17:26:15 +0200

    Add basic endpoint for role revoking

--- services/auth/src/api/v1/roles.py
+++ services/auth/src/api/v1/roles.py
@@ -62,8 +62,52 @@ async def assign_role(
     )
 
 
-# @router.patch(
-#     "/revoke",
-# )
-# async def revoke_role():
-#     pass
+@router.patch(
+    "/revoke",
+)
+async def revoke_role(
+    data: UserRole,
+    db: AsyncSession = Depends(get_session)
+) -> JSONResponse:
+    user_uuid = data.user_uuid
+
+    user = await db.execute(
+        select(User)
+        .where(User.uuid == user_uuid)
+        .options(selectinload(User.roles))
+    )
+    user = user.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"User with {user_uuid=} not found",
+        )
+
+    role_name = data.role.name
+    role = await db.get(Role, role_name)
+
+    if not role:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"Role with {role_name=} not found",
+        )
+
+    if role not in user.roles:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=f"User with {user_uuid=} does not have {role_name=}",
+        )
+
+    if len(user.roles) == 1:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=f"Cannot revoke the last role from user with {user_uuid=}",
+        )
+
+    user.roles.remove(role)
+    await db.commit()
+    return JSONResponse(
+        content={"message": f"{role_name=} successfully revoked from user with {user_uuid=}"},
+        status_code=HTTPStatus.OK,
+    )
