e5957f51e384180430bdbfba8e3fe8cd2aee47bc
Ksenia Evteeva <evteeva.kb@gmail.com>
2025-04-25 10:44:40 +0200

    Revert "Add services"
    This reverts commit 7c65a4806fe462ffcbe848b8d0d6831ecc8240fd.

--- services/auth/src/api/v1/roles.py
+++ services/auth/src/api/v1/roles.py
@@ -7,12 +7,13 @@ from http import HTTPStatus
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
 
 from api.v1.request_models import UserRole
 from db.postgre import get_session
+from models.entity import Role, User
 from openapi.roles import AssignRole, RevokeRole
-from services.role import get_role_service, RoleService
-from services.user import get_user_service, UserService
 
 
 router = APIRouter()
@@ -26,16 +27,28 @@ router = APIRouter()
     responses=AssignRole.responses,
 )
 async def assign_role(
-    data: UserRole,
-    db: AsyncSession = Depends(get_session),
-    role_service: RoleService = Depends(get_role_service),
-    user_service: UserService = Depends(get_user_service),
+    data: UserRole, db: AsyncSession = Depends(get_session)
 ) -> JSONResponse:
     user_uuid = data.user_uuid
-    user = await user_service.get_user_with_roles(user_uuid=user_uuid)
+
+    user = await db.execute(
+        select(User).where(User.uuid == user_uuid).options(selectinload(User.roles))
+    )
+    user = user.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"User with {user_uuid=} not found",
+        )
 
     role_name = data.role.name
-    role = await role_service.get_role(role_name)
+    role = await db.get(Role, role_name)
+    if not role:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"Role with {role_name=} not found",
+        )
     if role in user.roles:
         raise HTTPException(
             status_code=HTTPStatus.BAD_REQUEST,
@@ -60,16 +73,29 @@ async def assign_role(
     responses=RevokeRole.responses,
 )
 async def revoke_role(
-    data: UserRole,
-    db: AsyncSession = Depends(get_session),
-    role_service: RoleService = Depends(get_role_service),
-    user_service: UserService = Depends(get_user_service),
+    data: UserRole, db: AsyncSession = Depends(get_session)
 ) -> JSONResponse:
     user_uuid = data.user_uuid
-    user = await user_service.get_user_with_roles(user_uuid=user_uuid)
+
+    user = await db.execute(
+        select(User).where(User.uuid == user_uuid).options(selectinload(User.roles))
+    )
+    user = user.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"User with {user_uuid=} not found",
+        )
 
     role_name = data.role.name
-    role = await role_service.get_role(role_name)
+    role = await db.get(Role, role_name)
+
+    if not role:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"Role with {role_name=} not found",
+        )
 
     if role not in user.roles:
         raise HTTPException(
