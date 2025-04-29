cd37eb5b434ef0dac54b24b2c680fb958dce9d1d
denis <rden2000@gmail.com>
2025-04-29 04:18:20 +0300

    Fixes

--- services/auth/src/create_superuser.py
+++ services/auth/src/create_superuser.py
@@ -12,27 +12,29 @@ app = typer.Typer()
 
 async def create_superuser(email: str, password: str, session_gen = get_session()):
     session = await anext(session_gen)
-    result = await session.execute(select(User).where(User.email == email))
-    if result.scalar_one_or_none():
-        raise HTTPException(status_code=400, detail="User already exists")
+    try:
+        result = await session.execute(select(User).where(User.email == email))
+        if result.scalar_one_or_none():
+            raise Exception("User already exists")
 
-    role_result = await session.execute(select(Role).where(Role.name == "superuser"))
-    user_role = role_result.scalar_one_or_none()
+        role_result = await session.execute(select(Role).where(Role.name == "superuser"))
+        user_role = role_result.scalar_one_or_none()
 
-    if not user_role:
-        user_role = Role(name="superuser")
-        session.add(user_role)
+        if not user_role:
+            user_role = Role(name="superuser")
+            session.add(user_role)
+            await session.commit()
+            await session.refresh(user_role)
+
+        user = User(email=email, password=password)
+        user.is_active = True
+        user.roles = [user_role]
+
+        session.add(user)
         await session.commit()
-        await session.refresh(user_role)
-
-    user = User(email=email, password=password)
-    user.is_active = True
-    user.roles = [user_role]
-
-    session.add(user)
-    await session.commit()
-    await session.refresh(user)
-
+        await session.refresh(user)
+    finally:
+        await session.close()
 
 @app.command()
 def create(email: str, password: str):
