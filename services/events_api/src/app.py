4b971c7b7e0e3611184a18e15f50a820f93ff0d1
Ksenia Evteeva <evteeva.kb@gmail.com>
2025-05-26 17:43:43 +0200

    Add data validation to Flask

--- services/events_api/src/app.py
+++ services/events_api/src/app.py
@@ -1,11 +1,13 @@
 from http import HTTPStatus
 
 from flask import Flask, jsonify, Response
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
+ma = Marshmallow(app)
 
 
-@app.route("/api/health")  # type: ignore[misc]
+@app.route("/api/health", methods=["GET"])  # type: ignore[misc]
 def health() -> tuple[Response, int]:
     return jsonify(status="healthy"), HTTPStatus.OK
