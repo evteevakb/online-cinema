ac9fb1f9c40aaf1f3b85ae2e02d28ec229b5a8bd
Ksenia Evteeva <evteeva.kb@gmail.com>
2025-05-27 14:06:19 +0200

    Refactoring

--- services/events_api/src/app.py
+++ services/events_api/src/app.py
@@ -1,3 +1,7 @@
+"""
+Flask application for events collection.
+"""
+
 from http import HTTPStatus
 
 from flask import Flask, jsonify, request, Response
@@ -5,23 +9,38 @@ from flask_marshmallow import Marshmallow
 from kafka.errors import KafkaError
 from marshmallow import ValidationError
 
+# TODO: update isort settings if config and kafka_utils remain in the root dir
+from config import kafka_settings
 from schemas import kafka_message_schema
-from kafka_utils import KafkaTopicManager, KafkaProducerClient
+from kafka_utils import KafkaProducerClient, KafkaTopicManager
 
 
 app = Flask(__name__)
 ma = Marshmallow(app)
 
-BOOTSTRAP_SERVERS = "kafka-node-0:9092,kafka-node-1:9092,kafka-node-2:9092"
 
-
-@app.route("/api/health", methods=["GET"])  # type: ignore[misc]
+@app.route("/api/health", methods=["GET"])
 def health() -> tuple[Response, int]:
+    """Health check endpoint.
+
+    Returns:
+        tuple: A tuple containing a JSON response with status.
+    """
     return jsonify(status="healthy"), HTTPStatus.OK
 
 
 @app.route("/api/v1/kafka/publish", methods=["POST"])
-def publish_message():
+def publish_message() -> tuple[Response, int]:
+    """Publishes a validated JSON message to a Kafka topic.
+
+    Returns:
+        tuple: JSON response indicating success or failure, along with the corresponding HTTP status code.
+
+    Raises:
+        ValidationError: If the input JSON fails schema validation.
+        KafkaError: If an error occurs during Kafka communication.
+        Exception: For unexpected internal errors.
+    """
     json_data = request.get_json()
     if not json_data:
         return jsonify({"error": "No input data provided"}), HTTPStatus.BAD_REQUEST
@@ -31,19 +50,33 @@ def publish_message():
     except ValidationError as err:
         return jsonify({"errors": err.messages}), HTTPStatus.UNPROCESSABLE_CONTENT
 
-    app.logger.warning(f"INPUT DATA: {data}")
     try:
-        with KafkaTopicManager(bootstrap_servers=BOOTSTRAP_SERVERS) as manager:
-            if not manager.topic_exists(topic_name=data["topic"]):
-                manager.create_topic(topic_name=data["topic"])
+        with KafkaTopicManager(
+            bootstrap_servers=kafka_settings.bootstrap_servers
+        ) as manager:
+            if not manager.topic_exists(topic_name=kafka_settings.topic_name):
+                manager.create_topic(
+                    topic_name=kafka_settings.topic_name,
+                    num_partitions=kafka_settings.topic_num_partitions,
+                    replication_factor=kafka_settings.topic_replication_factor,
+                )
 
-        with KafkaProducerClient(BOOTSTRAP_SERVERS, data["topic"]) as producer:
+        with KafkaProducerClient(
+            bootstrap_servers=kafka_settings.bootstrap_servers,
+            topic=kafka_settings.topic_name,
+        ) as producer:
             key = data.get("key")
-            producer.send(value=data["value"].encode(), key=key.encode() if key else None)
-        return jsonify({"status": "Message sent successfully."}), 200
+            producer.send(
+                value=data["value"].encode(), key=key.encode() if key else None
+            )
+        return jsonify({"status": "Message sent successfully."}), HTTPStatus.OK
 
     except KafkaError as e:
         # TODO: handle NoBrokersAvailable
-        return jsonify({"error": f"Kafka error: {str(e)}"}), 500
+        return jsonify(
+            {"error": f"Kafka error: {str(e)}"}
+        ), HTTPStatus.INTERNAL_SERVER_ERROR
     except Exception as e:
-        return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
+        return jsonify(
+            {"error": f"Unexpected error: {str(e)}"}
+        ), HTTPStatus.INTERNAL_SERVER_ERROR
--- services/events_api/src/wsgi_app.py
+++ services/events_api/src/wsgi_app.py
@@ -3,6 +3,7 @@ Module to initialize the application with gevent monkey patching.
 """
 
 from gevent import monkey
+
 monkey.patch_all()
 
 from app import app
